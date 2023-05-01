4d11b8e5b49cbf6bd61cc7e3a042a17e065d39b4
pksnowstorm <enguyen21@gmail.com>
2023-05-01 11:24:13 -0500

    Complete log in and sign up interface

--- circleup/main_app/views.py
+++ circleup/main_app/views.py
@@ -16,4 +16,18 @@ def home(request):
     return HttpResponse('<h1> Hello /ᐠ｡‸｡ᐟ\ﾉ </h1>')
 
 def about(request):
-    return HttpResponse('<h1> About /ᐠ｡‸｡ᐟ\ﾉ </h1>')
+    return render(request, 'about.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid signup - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
