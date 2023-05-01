059cc9405b4f8f4d49e2ac7eb2ef1a5704e1d8c0
pksnowstorm <enguyen21@gmail.com>
2023-05-01 13:36:45 -0500

    Complete detail and index interface

--- circleup/main_app/models.py
+++ circleup/main_app/models.py
@@ -4,5 +4,15 @@ from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Circle(models.Model):
+    title = models.CharField(max_length=100)
+    creator = models.CharField(max_length=100)
+    link = models.URLField(max_length=500)
+    description = models.TextField()
+    tags = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+    allowed = models.ManyToManyField(User, related_name='allowed')  
+    requested = models.ManyToManyField(User, related_name='requested')
 
-
+    def __str__(self):
+        return self.title
